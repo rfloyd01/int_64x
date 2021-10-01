2d997da93b369770b716c06c1ffdf5377b2a537d
Robert Floyd <70585609+rfloyd01@users.noreply.github.com>
2021-10-01 17:28:22 -0400

    Update int_64x.h

--- int_64x.h
+++ int_64x.h
@@ -4,124 +4,129 @@
 #include <vector>
 #include <string>
 
-//The int_64x class is my attempt at making an integer type of arbitrary length. This
-//is accomplished by stringing together multiple (although not necessarily consecutive)
-//unsigned long long int types together to make a single cohesive number. Instead of
-//adding an entire int_64x to another int_64x type, the individual unsigned long longs
-//that make up each number are manipulated individually and then are brought back 
-//together to form a new int_64x type
+//The int_64 class is my attempt at making an integer type of arbitrary length. This
+//is accomplished by stringing together unsigned long long types stored in a vector
+//which handles the deconstruction. By stringing together multiple 64-bit numbers
+//it's possible to make numbers that are 128-bit, 256-bit, etc. up to any arbitrary
+//size. This class utilizes built in functions to manipulate these large numbers as
+//much as possible and then employs custom algorithms to handle certain things that the
+//built in arithmetic functions can't, like calculating the carry between additions. The
+//goal of this class was to, first and foremost, learn about overloading operators for a
+//custom class, but also to make a type for C++ that can handle large numbers ala Python.
+//I've come across numerous libraries online with this goal, however, most of the one's
+//that I've found either use a vector of integers to represent long numbers or a string
+//to represent long numbers. The vector of integers method can be quite space intensive
+//(a 500 digit number for example would need 32-bits * 500 digits = 2kB of storage) while
+//the string method takes a very long time to carry out calculations as each character
+//needs to be converted to an integer before it can be manipulated. This class will save
+//on both space and time by keeping large numbers in a binary representation and splitting
+//the number up between multiple 64-bit words (for example the number 9,223,372,036,854,775,808
+//would be represented as [0x0000000000000001], [0x0000000000000000]).
 
-//This class will be able to handle numbers of any size as long as it fits in the user's
-//memory.
+//TODO:
+//1. The division operator needs to be optimized (haven't attempted this yet so there should be some low hanging fruit here).
+//2. >>= operator needs to have the same logic as the <<= operator
+//3. Need to add boolean AND, NOT and XOR operators
+//4. String initialization and printing need to be optimized
+//5. Add versions of each operator that work directly on unsigned longs, should be quicker than converting to int_64x
 
-//Time Comparisons
+//Arithmetic Operator Time Comparisons with a Similar Class
+//These are time comparisons with another C++ big integer type library that I had initially used from online somewhere.
+//The int_64x class probably isn't the fastest big integer class out there but it's nice to have a comparison. I tested
+//each operator for speed using either the 5 digit numbers 12345 and 54321, or by using the 100 digit numbers
+//1234567890123... and 9876543210987...
+//----------------
+//+= operator: ~37x faster for 5 digit numbers,  ~14x faster for 100 digit numbers
+//+  operator: ~ 2x faster for 5 digit numbers,  ~ 4x faster for 100 digit numbers
+//-= operator: ~56x faster for 5 digit numbers,  ~20x faster for 100 digit numbers
+//-  operator: ~ 4x faster for 5 digit numbers,  ~ 3x faster for 100 digit numbers
+//*= operator: ~49x faster for 5 digit numbers, ~177x faster for 100 digit numbers
+//*  operator: ~ 2x faster for 5 digit numbers,  ~27x faster for 100 digit numbers (something is screwy here for the 5 digit case)
+///= operator: ~.5x faster for 5 digit numbers,  ~.3x faster for 100 digit numbers
+// / operator: ~ 6x faster for 5 digit numbers,  ~12x faster for 100 digit numbers
 //----------------
 
 class int_64x
 {
 public:
-	int_64x();  //default constructor
-	~int_64x(); //default destructor
-
-	int_64x(int number); //create a new int_64x type from a signed integer
-	int_64x(unsigned int number); //create a new int_64x type from an unsigned integer
-	int_64x(long long number); //create a new int_64x type from a signed long long integer
-	int_64x(unsigned long long number); //create a new int_64x type from an unsigned long long integer
-	int_64x(unsigned long long number, int left_shift); //create a new int_64x type by left shifting the given unsigned long long
-	int_64x(std::string number); //create a new int_64x type via a string of numbers, the best way for instantiating a long variable
-	int_64x(int zero, int number_longs); //create a new int_64x type that has "number_longs" long longs all initialized to 0
-	int_64x(const int_64x& num); //Copy constructor
-
-	//std::vector<unsigned long long*> getDigits();
-
-	//Arithmetic Operators
-	int_64x& operator+=(const int_64x& num);
-	int_64x& operator+=(const unsigned long long& num); //TODO: this should be updated to match new += operator
-	int_64x& operator-=(const int_64x& num);
-	int_64x& operator-=(const unsigned long long& num); //TODO: this should be updated to match new -= operator
-	//int_64x& operator*=(int_64x& num); //TODO: Do what's necessary to change "num" to a const (and try to apply Katsuba)
-	int_64x& operator*=(const int_64x& num);
-	int_64x& operator*=(int num);
-	int_64x& operator/=(int_64x& num); //TODO: Do what's necessary to change "num" to a const
-	//int_64x& operator/=(const unsigned long long& num); //TODO: Come back to this at some point
-	int_64x operator/(int_64x& num);
-	void FastMultiplication(const int_64x& num); //this was supposed to be a much quicker alternative to normal addition, but it's only a few % quicker
-
-	//Binary Operators
-	//TODO: I'm not complete satisfied with the <<= operater, see if it can be reduced at all. Maybe only focus on positive numbers
-	int_64x& operator<<=(const unsigned int left_shift);
-	int_64x& operator>>=(const unsigned int left_shift); //TODO: Update so this has same logic as new <<= operator
-	int_64x& operator |= (int_64x& num);
-
-	int_64x& operator=(const int_64x& num);
-	
-	std::string getNumberString();
+	//VARIABLES
 
 	//Digits holds the binary equivalent of our number. In order to acheive an arbitrary length a vector is utilized.
 	//The digits are in Big Endian format, however, the 64-bit word order is reversed. So a 192 bit word would be ordered like so (where
 	//0 is the LSB and 191 is the MSB):
 	//[63, 62, 61, ... 0], [127, 126, 125 ... 64], [191, 190, 189, ... 128]
-	std::vector<unsigned long long*> digits;
+	std::vector<unsigned long long> digits;
+
+	//CONSTRUCTORS
+	int_64x() = default; //no special default operator
+	int_64x(int number); //create a new int_64x type from a signed integer
+	int_64x(unsigned int number); //create a new int_64x type from an unsigned integer
+	int_64x(long long number); //create a new int_64x type from a signed long long integer
+	int_64x(unsigned long long number, int left_shift = 0); //create a new int_64x type by left shifting the given unsigned long long
+	int_64x(std::string number); //create a new int_64x type via a string of numbers, the best way for instantiating a long variable
+	int_64x(const int_64x& num); //Copy constructor
+
+	//ARITHMETIC OPERATORS
+	//Addition Operators
+	int_64x& operator+=(const int_64x& num);
+	friend int_64x operator+(const int_64x& num1, const int_64x& num2);
+
+	//Subtraction Operators
+	int_64x& operator-=(const int_64x& num);
+	friend int_64x operator-(const int_64x& num1, const int_64x& num2);
+
+	//Multiplication Operators
+	int_64x& operator*=(const int_64x& num);
+	friend int_64x operator*(const int_64x& num1, const int_64x& num2); //TODO: This operator is going really slow with small numbers but not big numbers, investigate and find out why
+	void FastMultiplication(const int_64x& num);
+
+	//Division Operators
+	int_64x& operator/=(const int_64x& num); //TODO: This operator hasn't yet been optimized, come back at some point and spend some time on this
+	friend int_64x operator/(const int_64x& num1, int_64x& num2);
+
+	//BINARY OPERATORS
+	//Left Shift Operators
+	int_64x& operator<<=(const unsigned int left_shift);
+	friend int_64x operator<<(const int_64x& num, const unsigned int left_shift);
+
+	//Right Shift Operators
+	int_64x& operator>>=(const unsigned int left_shift); //TODO: Update so this has same logic as <<= operator
+	friend int_64x operator>>(const int_64x& num, const unsigned int right_shift);
+
+	//OR Operators
+	int_64x& operator |= (const int_64x& num);
+	friend int_64x operator|(const int_64x& num1, const int_64x& num2);
+
+	//TODO: add AND operators
+
+	//BOOLEAN OPERATORS
+	friend bool operator==(const int_64x& num1, const int_64x& num2);
+	friend bool operator!=(const int_64x& num1, const int_64x& num2);
+	friend bool operator<(const int_64x& num1, const int_64x& num2);
+	friend bool operator>(const int_64x& num1, const int_64x& num2);
+	friend bool operator<=(const int_64x& num1, const int_64x& num2);
+	friend bool operator>=(const int_64x& num1, const int_64x& num2);
+
+	//Assignment Operators
+	int_64x& operator=(const int_64x& num);
+	
+	//OTHER FUNCTIONS
+	std::string getNumberString();
 	void partialAddition(unsigned long long num, int word); //make this private after testing
-private:
 	void zero();
 };
 
-//Helper Functions and Operators
+//Related Functions
 bool CompareArraySize(int* one, int* two, int elements);
 void MultiplyArrayByTwo(int* numbers, int elements);
 void DivideArrayByTwo(int* numbers, int elements);
 void AddArrays(int* one, int* two, int elements);
 void SubtractArrays(int* one, int* two, int elements);
-//void PrintBinary(int_64x& num);
-void PrintBinary(int_64x num);
 void twosComplement(int_64x& num);
-int_64x flipBits(int_64x& num);
 int fastlog2(unsigned long long value);
 int GetLeadBitLocation(int_64x &num);
-int_64x Karatsuba(const std::vector<unsigned long long> num1, const std::vector<unsigned long long> num2, unsigned long long num1_polarity, unsigned long long num2_polarity);
 void unsignedAddition(unsigned long long* num1, unsigned long long num2, int num1_size, int word);
 
-//Can print int_64x types by reference or copy
+//Printing Functions
 std::ostream& operator<<(std::ostream& os, int_64x& num);
-
-//Addition operators allow for addition of int_64x types with unsigned or signed long long and ints
-//as well as other int_64x types with implicit conversion
-//TODO: It would be great if all of these could be combined into a single function, look int this at some point
-//int_64x operator+(int_64x& num1, int_64x& num2);
-
-//Friendly Arithmetic Operators
-int_64x operator+(const int_64x& num1, const int_64x& num2);
-int_64x operator-(int_64x num1, int_64x& num2);
-int_64x operator-(int_64x num1, int& num2);
-int_64x operator*(int_64x& num1, int_64x& num2);
-int_64x operator*(int_64x num1, int num2);
-
-//Friendly Binary Operators
-int_64x operator<<(int_64x num, const unsigned int left_shift);
-int_64x operator>>(int_64x& num, const unsigned int right_shift);
-int_64x operator|(int_64x num1, int_64x& num2);
-
-//Friendly Comparison Operators
-bool operator==(const int_64x& num1, const int_64x& num2);
-bool operator!=(const int_64x& num1, const int_64x& num2);
-bool operator<(const int_64x& num1, const int_64x& num2);
-bool operator>(const int_64x& num1, const int_64x& num2);
-bool operator<=(const int_64x& num1, const int_64x& num2);
-bool operator>=(const int_64x& num1, const int_64x& num2);
-
-//NOTES
-//Currently to create and destroy an instance from a built-int types takes 9.31e-8 seconds, creating 1,000,000 takes 0.093 seconds
-//1,000,000 normal built in types can be created in 3e-7 seconds. It probably is so much longer for the int_64x's because there's a
-//vector which needs to be created and deleted. Vectors are 24 bytes where's the maximum built-in types is only 8 bytes so I'd expect
-//it to take ~4 times longer to create and destroy the int_64x type based on number of things getting created. The 0.093 seconds feels
-//like it's longer than it should be currently. Maybe in the future I wont use a vector and just link all of the 64-bit words of the
-//int_64x type with pointers in a linked list fashion.
-
-//Currently to create and destroy an instance from a string takes 1.61e-6 seconds, creating 1,000,000 takes 1.61 seconds. This is
-//obviously much slower than creating from one of the built in types but that's to be expected as there's a lot more entailed in
-//doing it this way. This method is really only intended for creating instances for numbers that are already really large which isn't
-//possible with a built in type. I can probably make it more efficient to create int_64x types in this manner, however, the object
-//is less about creating large numbers from the get-go and more about building up to large numbers through arithmetic.
-
-//I'll need to find a more efficient way of creating and destroying instances of this class at some point
+void PrintBinary(int_64x num);
